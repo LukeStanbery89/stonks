1ecc03e05aa6dc6d0275fb896caf674cb76ffd2e
Luke Stanbery <lukestanbery89@gmail.com>
2022-02-01 20:14:51 -0600

    Refactor broker config into ES module

---
+++ server/src/trade/broker/broker.config.js
@@ -0,0 +1,9 @@
+export default {
+    commands: {
+        BUY: 'buy',
+        SELL: 'sell',
+        GET_POSITIONS: 'getPositions',
+        GET_POSITION: 'getPosition',
+        GET_ACCOUNT_INFO: 'getAccountInfo'
+    }
+};
