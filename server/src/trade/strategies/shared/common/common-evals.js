c9316ddfc40a554833874d9c4d2f8799a9e70650
Luke Stanbery <lukestanbery89@gmail.com>
2022-02-04 09:36:00 -0600

    Implement open orders eval functions and write tests

--- server/src/trade/strategies/shared/common/common-evals.js
+++ server/src/trade/strategies/shared/common/common-evals.js
@@ -5,3 +5,29 @@ export function omitBlacklistedSecurities(securityData) {
         return resolve(!tradeConfig.blacklist.includes(securityData.symbol));
     });
 }
+
+export function noOpenOrder(securityData, aggregate) {
+    return new Promise(resolve => {
+        return resolve(aggregate?.orders?.filter(order => {
+            return order.symbol === securityData.symbol;
+        }).length === 0);
+    });
+}
+
+export function noOpenBuyOrder(securityData, aggregate) {
+    return new Promise(resolve => {
+        return resolve(aggregate?.orders?.filter(order => {
+            return order.symbol === securityData.symbol
+                && order.side.toLowerCase() === 'buy';
+        }).length === 0);
+    });
+}
+
+export function noOpenSellOrder(securityData, aggregate) {
+    return new Promise(resolve => {
+        return resolve(aggregate?.orders?.filter(order => {
+            return order.symbol === securityData.symbol
+                && order.side.toLowerCase() === 'sell';
+        }).length === 0);
+    });
+}
